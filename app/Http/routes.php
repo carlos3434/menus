e51b37479609b02304b58747b9f6a60d339bff30
juan <carlos3434@hotmail.com>
2016-10-05 22:51:46 -0500

    vue-table

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,13 +10,47 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-/*
+
 use Restaurant\Role;
 use Restaurant\Permission;
 use Restaurant\User;
 Route::get('roles', function () {
     return User::all();
-});*/
+});
+
+Route::get('/api/users2', function() { //dd("22");
+    return User::paginate(10);
+    $request = request();
+
+    // handle sort option
+    if (request()->has('sort')) {
+        list($sortCol, $sortDir) = explode('|', request()->sort);
+        $query = User::orderBy($sortCol, $sortDir);
+    } else {
+        $query = User::orderBy('id', 'asc');
+    }
+
+    if ($request->exists('filter')) {
+        $query->where(function($q) use($request) {
+            $value = "%{$request->filter}%";
+            $q->where('name', 'like', $value)
+                ->orWhere('nickname', 'like', $value)
+                ->orWhere('email', 'like', $value);
+        });
+    }
+
+    $perPage = request()->has('per_page') ? (int) request()->per_page : null;
+
+    // The headers 'Access-Control-Allow-Origin' and 'Access-Control-Allow-Methods'
+    // are to allow you to call this from any domain (see CORS for more info).
+    // This is for local testing only. You should not do this in production server,
+    // unless you know what it means.
+    return response()->json(
+        $query->paginate($perPage)
+    )
+    ->header('Access-Control-Allow-Origin', '*')
+    ->header('Access-Control-Allow-Methods', 'GET');
+});
 
 Route::group(array('domain' => 'menus'), function() {
     Route::get('mi/ruta', function() {
@@ -38,6 +72,7 @@ Route::group(array('domain' => 'tercera.menus'), function(){
 
 
 Route::resource('/usuarios', 'UserController');
+Route::resource('/preparacion', 'PreparacionController');
 Route::resource('/pedidos', 'PedidoController');
 Route::resource('/api/users', 'ApiUserController');
 
