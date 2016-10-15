390f72c2bc3fc21996fd0e5e21336150a4a03e74
juan <carlos3434@hotmail.com>
2016-10-14 22:00:21 -0500

    heroku

--- config/app.php
+++ config/app.php
@@ -26,7 +26,8 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    //'debug' => env('APP_DEBUG', false),
+    'debug' => true,
 
     /*
     |--------------------------------------------------------------------------
