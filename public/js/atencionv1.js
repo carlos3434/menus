69e7d00e792e22b5275f4d1cbc7165eece33d43e
Flavio Huanacchiri <limawebperu@gmail.com>
2016-10-16 10:38:05 -0500

    v1

--- public/js/atencionv1.js
+++ public/js/atencionv1.js
@@ -1,4 +1,5 @@
 localStorage.setItem("productosS", "{}");
+localStorage.setItem("productosR", "{}");
 
 $(".btn-mesa").click(function(){
 	console.log($(this).data("mesa"));
@@ -21,19 +22,46 @@ $(".btn-agregar-producto").click(function(){
 	localStorage.setItem("productosS", JSON.stringify(seleccionados));
 });
 
+$(".btn-remover-producto").click(function(){
+	seleccionados = [];
+	$(".btn-producto").each(function(){
+		console.log($(this));
+		if($(this).is(":checked")) {
+			seleccionados.push($(this).val());
+		}
+	});
+	pintarResultado(seleccionados);
+	localStorage.setItem("productosR", JSON.stringify(seleccionados));
+});
+
 pintarResultado = function(data) {
 	if (data.length > 0) {
 			html = "";
 		for(var i in data) {
 			valor = JSON.parse(data[i]);
+			valor.seleccionado = 1;
+			valor.stockS = 1;
 			id = valor.id;
 			stock = parseInt(valor.stock);
-			console.log(valor);
-			html+='<li><input type="checkbox" value="'+JSON.stringify(valor)+'" class="btn-producto" name="producto[]"><span class="descripcion">'+valor.descripcion_corta+'</span><span class="stock">1</span></li>';
+			html+='<li id="add-producto-'+id+'"><input type="checkbox" value="'+JSON.stringify(valor)+'" class="btn-producto" name="producto[]"><span class="descripcion">'+valor.descripcion_corta+'</span><span class="stock">1</span></li>';
 			nuevostock = stock-1;
 			$("#"+id+" span.stock").html(nuevostock);
 		}
 		$(".resultado").html(html);
 	}
+};
 
+pintarRemocion = function(data) {
+	if (data.length > 0) {
+		for(var i in data) {
+			valor = JSON.parse(data[i]);
+			stockS = parseInt(valor.stockS);
+
+			id = valor.id;
+			stock = parseInt($("li#"+id+" span.stock").html());
+			stock+=stockS;
+			$("li#"+id+" span.stock").html(stock);
+			$("#add-producto-"+id).remove();
+		}
+	}
 };
