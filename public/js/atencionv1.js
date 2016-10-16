1bf55dc9900186c5f1da150d48e8b5f5ec649425
Flavio Huanacchiri <limawebperu@gmail.com>
2016-10-16 09:05:41 -0500

    v1

--- public/js/atencionv1.js
+++ public/js/atencionv1.js
@@ -1,3 +1,5 @@
+localStorage.setItem("productosS", "{}");
+
 $(".btn-mesa").click(function(){
 	console.log($(this).data("mesa"));
 	data = $(this).data("mesa")
@@ -8,7 +10,21 @@ $(".btn-mesa").click(function(){
 });
 
 $(".btn-agregar-producto").click(function(){
+	seleccionados = [];
 	$(".btn-producto").each(function(){
 		console.log($(this));
+		if($(this).is(":checked")) {
+			seleccionados.push($(this));
+		}
 	});
+	pintarResultado(seleccionados);
+	localStorage.setItem("productosS", JSON.stringify(seleccionados));
 });
+
+pintarResultado = function(data) {
+	html = "";
+	for(var i in data) {
+		html+='<li><input type="checkbox" value="{&quot;descripcion_corta&quot;:&quot;Sopa + Pollo Saltado&quot;,&quot;preparacion&quot;:&quot;Esta es una descripcion de preparacion&quot;,&quot;stock&quot;:50,&quot;tipo&quot;:&quot;M&quot;}" class="btn-producto" name="producto[]"><span class="descripcion">Sopa + Pollo Saltado</span><span class="stock">50</span></li>';
+	}
+	$(".resultado").html(html);
+};
